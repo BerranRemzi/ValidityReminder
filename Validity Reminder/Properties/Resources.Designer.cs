1e6600464bb9cadde05055f384f92c048df82199
Berran Remzi <berranremzi@gmail.com>
2019-08-20 00:46:24 +0300

    Deleted print icon from

--- Validity Reminder/Properties/Resources.Designer.cs
+++ Validity Reminder/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace Validity_Reminder.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap PrintIcon {
-            get {
-                object obj = ResourceManager.GetObject("PrintIcon", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
